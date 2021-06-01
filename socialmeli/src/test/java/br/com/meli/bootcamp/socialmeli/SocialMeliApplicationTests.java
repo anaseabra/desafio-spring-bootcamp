eb641eddea2c5d8be57b1987c599772a5b3a2219
Ana Barbara Cacheado Seabra <ana.cacheado@mercadolivre.com>
2021-06-01 15:26:12 -0300

    first commit

---
+++ socialmeli/src/test/java/br/com/meli/bootcamp/socialmeli/SocialMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.bootcamp.socialmeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
